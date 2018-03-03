65aa3110f38099fc8756e275f4babfa9ac48db58
fanhuamancheng <741710840@qq.com>
2018-03-03 20:34:14 +0800

    退出功能完成

--- public/pc/js/common.js
+++ public/pc/js/common.js
@@ -13,7 +13,47 @@ $(function () {
     });
     $(document).ajaxStop(function () {
         NProgress.done();
-    })
+    });
 
+    // 发送请求检查是否登录,前提是当前页面不是登录页
+    if (location.href.indexOf('login.html') == -1) {
+        $.ajax({
+            type: 'get',
+            url: '/employee/checkRootLogin',
+            success: function (info) {
+                // console.log(info);
+                if (info.error == 400) {
+                    location.href = 'login.html';
+                }
+            }
+        });
+    }
+
+
+    // 二级菜单切换显示与隐藏
+    $('.second').prev().on('click', function () {
+        $(this).next().slideToggle();
+    });
+    $('.icon_menu').on('click', function () {
+        $('.lt_aside').toggleClass('now');
+        $('.lt_main').toggleClass('now');
+    });
+    // 退出功能
+    $('.icon_logout').on('click', function () {
+        // console.log(1);
+        $('#Modal').modal('toggle');
+    });
+    $('.btn_logout').on('click', function () {
+        $.ajax({
+            type: 'get',
+            url: '/employee/employeeLogout',
+            success: function (info) {
+                // console.log(info);
+                if (info.success) {
+                    location.href = 'login.html';
+                }
+            }
+        })
+    })
 
 });
